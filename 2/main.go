6e537e3c9388b98cedab87905e016cae2e0f97b7
Maria <zaush@bk.ru>
2023-09-06 13:57:31 +0300

    solved 2nd exercise

---
+++ 2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Написать программу, которая конкурентно рассчитает значение квадратов чисел взятых из массива (2,4,6,8,10) и выведет их квадраты в stdout.
+
+func main() {
+	arr := []int{2, 4, 6, 8, 10}
+
+	calculateSquare(arr)
+}
+
+func calculateSquare(arr []int) {
+	var wg sync.WaitGroup
+
+	for _, val := range arr {
+		wg.Add(1)
+		go func(item int) {
+			fmt.Println(item * item)
+			wg.Done()
+		}(val)
+		wg.Wait()
+	}
+}
